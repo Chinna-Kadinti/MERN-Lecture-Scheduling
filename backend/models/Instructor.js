b0bdc2580f9fddbbf286304578b870598a63accc
Chinna Kadinti <157778675+Chinna-Kadinti@users.noreply.github.com>
2025-04-12 11:37:16 +0530

    Update Instructor.js

--- backend/models/Instructor.js
+++ backend/models/Instructor.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const instructorSchema = new mongoose.Schema({
   name: { type: String, required: true },
-    phone: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
 });
 module.exports = mongoose.model('Instructor', instructorSchema);
